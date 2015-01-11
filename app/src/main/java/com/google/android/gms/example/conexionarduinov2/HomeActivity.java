c0a85becbe4fa3398d79802f3b4d345d158580dc
Salvador <salvadoredgar99@gmail.com>
2015-01-10 18:17:43 -0600

    Add project

---
+++ app/src/main/java/com/google/android/gms/example/conexionarduinov2/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.google.android.gms.example.conexionarduinov2;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class HomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+}
